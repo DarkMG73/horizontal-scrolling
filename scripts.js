1ced83dd3480e6b1506316c9d3bb919c370164f9
DarkMG73 <levelthreeemail@gmail.com>
2018-11-23 10:52:47 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 $( document).ready(function() {
 
+  $('#gi_horizontal #hz_inner-wrap > div').addClass('inline_div');
+
+  
+//var totalWidth = $('#inner-wrap > div').outerWidth;
+//console.log(totalWidth);
+
+
+var width = 0;
+$('#gi_horizontal #hz_inner-wrap').children().each(function() {
+    width += $(this).outerWidth( true );
+});
+
+  
+console.log(width);
+$('#gi_horizontal #hz_inner-wrap').css('width', width + 250);
+
 }); // END document.ready
